ec74621fc4338161c2d133090a0b8a6a0c8e43ce
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-06-18 06:14:38 +0000

    resize post_image

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,7 +2,7 @@ class PostCommentsController < ApplicationController
 
   def create
     post_image= PostImage.find(params[:post_image_id])
-    comment =currente_user.post_comments.new(post_comment_params)
+    comment =current_user.post_comments.new(post_comment_params)
     comment.save
     redirect_to post_image_path(post_image)
   end
